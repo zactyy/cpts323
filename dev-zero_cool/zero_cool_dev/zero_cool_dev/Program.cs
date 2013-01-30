54d6373b1445f55476836508c901541549e98f66
Zachary Tyynismaa <zachary.tyynismaa@email.wsu.edu>
2013-01-29 18:02:30 -0800

    zero cool modules solution
    used for dev's module development and testing

---
+++ dev-zero_cool/zero_cool_dev/zero_cool_dev/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zero_cool_dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This console and solution will be used for module development by dev alias zero cool");
+        }
+    }
+}
