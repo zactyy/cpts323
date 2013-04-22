31977dddf0af44bea329fad76af809efdce20e70
chris walters <zareth4@gmail.com>
2013-04-22 16:41:19 -0700

    video buttons again

--- project1/Asml-MHS/Asml-MHS/MainWindow.xaml.cs
+++ project1/Asml-MHS/Asml-MHS/MainWindow.xaml.cs
@@ -87,7 +87,7 @@ namespace Asml_McCallisterHomeSecurity
         private void btnVideoStop_Click(object sender, RoutedEventArgs e)
         {
             try{
-                _eye_of_sauron.Stop();
+                _eye_of_sauron.NewImage += on_image_changed;
             }
             catch (Exception ex)
             {
@@ -99,7 +99,7 @@ namespace Asml_McCallisterHomeSecurity
         {
             try
             {
-                _eye_of_sauron.Start();
+                _eye_of_sauron.NewImage -= on_image_changed;
             }
             catch (Exception ex)
             {
